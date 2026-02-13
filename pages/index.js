bf04a49b6d5298807023019ef418440d2758a4da
Pablo Pereira <pablopereira27@gmail.com>
2026-02-13 18:04:46 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>"Quando não há nada para se agarrar... Agarra-se a sua fé..."</h1>
+      <h2>- Talus, na série <strong>Knightfall</strong> <small>(Temporada 2, Episódio 3)</small></h2>
+    </>
+  );
+}
+
+export default Home;
